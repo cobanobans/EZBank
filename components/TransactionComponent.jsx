f1790a42d24ed160a5343f3fcf70a2030471a8e8
Djordje <djolej17@gmail.com>
2024-05-24 00:55:46 +0200

    trans component, px form

--- components/TransactionComponent.jsx
+++ components/TransactionComponent.jsx
@@ -4,7 +4,7 @@ import StatsComponent from './StatsComponent'
 const TransactionComponent = ({ type, userId, userEmail }) => {
   return (
     <div className='flex flex-col-reverse py-20 md:flex-row lg:w-full items-center justify-between bg-base-200 rounded-xl shadow-xl gap-10 lg:gap-5'>
-      <div className='w-full lg:w-2/3 px-10'>
+      <div className='w-full lg:w-2/3 md:px-10'>
         <NewTransactionForm type={type} userEmail={userEmail} userId={userId} />
       </div>
       <div className='px-10'>
