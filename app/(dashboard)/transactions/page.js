1fd33ca979f109cc958e415e3aedd2a216771d9d
Djordje <djolej17@gmail.com>
2024-05-24 00:50:36 +0200

    transactions page - stats centered

--- app/(dashboard)/transactions/page.js
+++ app/(dashboard)/transactions/page.js
@@ -6,7 +6,7 @@ const TransactionsPage = async () => {
   // const { highlightLast } = router.query
   const { userId } = auth()
   return (
-    <div className='flex flex-col items-start lg:items-center w-full lg:px-20'>
+    <div className='flex flex-col items-start items-center w-full lg:px-20'>
       <StatsComponent userId={userId} />
       <div className='mt-10 w-full'>
         <AllTransactions userId={userId} />
